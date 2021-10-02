15a39c319a6af4c5509c3d9264499808f5d0355b
Lucas Heim <lucasedfc@gmail.com>
2021-10-02 16:59:11 -0300

    Module 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name: string = 'lucasedfc';
+  age: number = 31;
+  img: string = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+
+  person = {
+    name: 'lucasedfc',
+    age: 31,
+    avatar: 'https://i.pravatar.cc/100'
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  addAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
