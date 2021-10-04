a587b5e19a4c621b033d4f60579d321e853687b7
Lucas Heim <lucasedfc@gmail.com>
2021-10-03 21:10:39 -0300

    Angular Fundamentals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red',
+  };
+
+  widthImg = 10;
   name: string = 'lucasedfc';
   age: number = 31;
   img: string = 'https://source.unsplash.com/random';
@@ -14,8 +22,51 @@ export class AppComponent {
   person = {
     name: 'lucasedfc',
     age: 31,
-    avatar: 'https://i.pravatar.cc/100'
-  }
+    avatar: 'https://i.pravatar.cc/100',
+  };
+
+  names: string[] = ['michael', 'john', 'pam', 'david', 'angela'];
+  newName = '';
+
+  products: Product[] = [
+    {
+      name: 'The best toy',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicycle',
+      price: 356,
+      image: './assets/images/bike.jpg',
+    },
+    {
+      name: 'Album Collection',
+      price: 34,
+      image: './assets/images/album.jpg',
+    },
+    {
+      name: 'My Books',
+      price: 23,
+      image: './assets/images/books.jpg',
+    },
+    {
+      name: 'Dog House',
+      price: 34,
+      image: './assets/images/house.jpg',
+    },
+    {
+      name: 'GLasses',
+      price: 3434,
+      image: './assets/images/glasses.jpg',
+    },
+  ];
+
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  };
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
@@ -33,5 +84,19 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  delName(index: number) {
+    this.names.splice(index, 1);
+  }
+
+  onRegister() {
+    console.log(this.register);
+
   }
 }
