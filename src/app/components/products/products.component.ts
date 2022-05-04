ff5b33eecf32592e0c1183ccd02903eaa465bfe0
Lucas Heim <lheim@merlindataquality.com>
2022-05-03 23:36:34 -0300

    Module API Rest

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product } from '../../models/product.model';
+import {
+  CreateProductDto,
+  Product,
+  UpdateProductDto,
+} from '../../models/product.model';
 
 import { StoreService } from '../../services/store.service';
 import { ProductsService } from '../../services/products.service';
@@ -8,13 +12,24 @@ import { ProductsService } from '../../services/products.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
   myShoppingCart: Product[] = [];
   total = 0;
   products: Product[] = [];
+  showProductDetail: boolean = false;
+  productChosen: Product = {
+    id: '',
+    price: 0,
+    images: [],
+    title: '',
+    category: {
+      id: '',
+      name: '',
+    },
+    description: '',
+  };
 
   constructor(
     private storeService: StoreService,
@@ -24,8 +39,7 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productsService.getAllProducts()
-    .subscribe(data => {
+    this.productsService.getAllProducts().subscribe((data) => {
       this.products = data;
     });
   }
@@ -35,4 +49,64 @@ export class ProductsComponent implements OnInit {
     this.total = this.storeService.getTotal();
   }
 
+  toggleProductDetail() {
+    this.showProductDetail = !this.showProductDetail;
+  }
+
+  onShowDetail(id: string) {
+    this.productsService.getProductById(id).subscribe({
+      next: (product: Product) => {
+        this.toggleProductDetail();
+        this.productChosen = product;
+      },
+    });
+  }
+
+  createNewProduct() {
+    const product: CreateProductDto = {
+      description: 'Lorem Ipsum sasda',
+      images: [`https://placeimg.com/640/480/any?random=${Math.random()}`],
+      price: 321,
+      title: 'New Product',
+      categoryId: 2,
+    };
+    this.productsService.create(product).subscribe({
+      next: (data: Product) => {
+        console.log('created', data);
+        this.products.unshift(data);
+      },
+    });
+  }
+
+  updateProduct() {
+    const changes: UpdateProductDto = {
+      title: 'Title Modified',
+      description: 'That is what she said!',
+    };
+
+    const { id } = this.productChosen;
+
+    this.productsService.update(id, changes).subscribe({
+      next: (data: Product) => {
+        const productIndex = this.products.findIndex(
+          (item) => item.id == this.productChosen.id
+        );
+        this.products[productIndex] = data;
+        this.productChosen = data;
+      },
+    });
+  }
+
+  deleteProduct() {
+    const { id } = this.productChosen;
+    this.productsService.delete(id).subscribe({
+      next: () => {
+        const productIndex = this.products.findIndex(
+          (item) => item.id === this.productChosen.id
+        );
+        this.products.splice(productIndex, 1);
+        this.showProductDetail = false;
+      },
+    });
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Product } from './../models/product.model';
+import { CreateProductDto, Product, UpdateProductDto } from './../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
+  private apiUrl: string = 'https://young-sands-07814.herokuapp.com/api/products';
+
   constructor(
     private http: HttpClient
   ) { }
 
   getAllProducts() {
-    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
+  getProductById(id: string) {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
+  create(data: CreateProductDto) {
+    return this.http.post<Product>(`${this.apiUrl}`, data);
+  }
+
+  update(id: string, data: UpdateProductDto) {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, data)
+  }
+
+  delete(id: string) {
+    return this.http.delete<boolean>(`${this.apiUrl}/${id}`);
   }
 }
