1b983588d8d0565bdc92948f6c79a020baa58347
Lucas Heim <lheim@merlindataquality.com>
2022-05-28 15:53:03 -0300

    Shared Module

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -3,12 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { WebsiteRoutingModule } from './website-routing.module';
 
-import { ImgComponent } from '.././website/components/img/img.component';
-import { ProductComponent } from '.././website/components/product/product.component';
-import { ProductsComponent } from '.././website/components/products/products.component';
 import { NavComponent } from '.././website/components/nav/nav.component';
-import { ReversePipe } from '.././website/pipes/reverse.pipe';
-import { TimeAgoPipe } from '.././website/pipes/time-ago.pipe';
 import { HomeComponent } from '.././website/pages/home/home.component';
 import { CategoryComponent } from '.././website/pages/category/category.component';
 import { MycartComponent } from '.././website/pages/mycart/mycart.component';
@@ -18,21 +13,14 @@ import { RecoveryComponent } from '.././website/pages/recovery/recovery.componen
 import { ProfileComponent } from '.././website/pages/profile/profile.component';
 import { ProductDetailComponent } from '.././website/pages/product-detail/product-detail.component';
 import { LayoutComponent } from '.././website/components/layout/layout.component';
-import { HighlightDirective } from '../website/directives/highlight.directive';
 import { SwiperModule } from 'swiper/angular';
-import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from '../shared/shared.module';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
     NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
     HomeComponent,
     CategoryComponent,
     MycartComponent,
@@ -49,7 +37,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     SwiperModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule,
+    SharedModule
   ],
 })
 export class WebsiteModule {}
